07d12d37558958964f38cdf3cec908995875b049
FlutterCrypto <fluttercrypto@gmx.de>
2022-03-26 19:16:55 +0100

    updated

--- app/src/main/java/de/androidcrypto/androidcommonintents/IntentGroup01.java
+++ app/src/main/java/de/androidcrypto/androidcommonintents/IntentGroup01.java
@@ -2,10 +2,18 @@ package de.androidcrypto.androidcommonintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class IntentGroup01 extends AppCompatActivity {
 
@@ -33,6 +41,39 @@ public class IntentGroup01 extends AppCompatActivity {
         btn01.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // create an alarm
+                // https://developer.android.com/guide/components/intents-common
+                // https://developer.android.com/reference/android/provider/AlarmClock#ACTION_SET_ALARM
+                // alarm clock
+                // create an alarm
+                // <uses-permission android:name="com.android.alarm.permission.SET_ALARM" />
+                /* <intent-filter>
+        <action android:name="android.intent.action.SET_ALARM" />
+        <category android:name="android.intent.category.DEFAULT" />
+    </intent-filter>
+
+                 */
+                //Calendar calendar;
+                //calendar.s
+
+                //Calendar alarm = new GregorianCalendar(TimeZone.getDefault());
+                //alarm.setTimeInMillis(calendar.getTimeInMillis());
+                //int hour = alarm.get(Calendar.HOUR_OF_DAY);
+                //int minutes = alarm.get(Calendar.MINUTE);
+                // |
+                Context context = v.getContext();
+                Intent alarmIntent = new Intent(AlarmClock.ACTION_SET_ALARM);
+                alarmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                //alarmIntent.putExtra(AlarmClock.EXTRA_MESSAGE, label);
+                alarmIntent.putExtra(AlarmClock.EXTRA_HOUR, 19);
+                alarmIntent.putExtra(AlarmClock.EXTRA_MINUTES, 35);
+                alarmIntent.putExtra(AlarmClock.EXTRA_MESSAGE, "Test alarm");
+                //alarmIntent.putExtra(AlarmClock.EXTRA_DAYS, Calendar.SATURDAY);
+
+                if (alarmIntent.resolveActivity(context.getPackageManager()) != null)
+                {
+                    context.startActivity(alarmIntent);
+                }
 
             }
         });
@@ -40,20 +81,70 @@ public class IntentGroup01 extends AppCompatActivity {
         btn02.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // show alarms
+                /*
+                <activity ...>
+    <intent-filter>
+        <action android:name="android.intent.action.SHOW_ALARMS" />
+        <category android:name="android.intent.category.DEFAULT" />
+    </intent-filter>
+</activity>
+                 */
+                Context context = v.getContext();
+                Intent alarmIntent = new Intent(AlarmClock.ACTION_SHOW_ALARMS);
 
+                if (alarmIntent.resolveActivity(context.getPackageManager()) != null)
+                {
+                    context.startActivity(alarmIntent);
+                }
             }
         });
 
         btn03.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // set a timer
+                // <uses-permission android:name="com.android.alarm.permission.SET_ALARM" />
+                /*
+<intent-filter>
+        <action android:name="android.intent.action.SET_TIMER" />
+        <category android:name="android.intent.category.DEFAULT" />
+    </intent-filter>
+                 */
 
+                Context context = v.getContext();
+                Intent alarmIntent = new Intent(AlarmClock.ACTION_SET_TIMER);
+                alarmIntent.putExtra(AlarmClock.EXTRA_LENGTH, 10); // 10 seconds
+                alarmIntent.putExtra(AlarmClock.EXTRA_MESSAGE, "Test timer");
+                //alarmIntent.putExtra(AlarmClock.EXTRA_DAYS, Calendar.SATURDAY);
+
+                if (alarmIntent.resolveActivity(context.getPackageManager()) != null)
+                {
+                    context.startActivity(alarmIntent);
+                }
             }
         });
 
         btn04.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // show all timers
+                /*
+                <intent-filter>
+        <action android:name="android.intent.action.SHOW_ALARMS" />
+        <category android:name="android.intent.category.DEFAULT" />
+    </intent-filter>
+                 */
+                Context context = v.getContext();
+                Intent alarmIntent = new Intent(AlarmClock.ACTION_SHOW_TIMERS);
+                //alarmIntent.putExtra(AlarmClock.EXTRA_LENGTH, 10); // 10 seconds
+                //alarmIntent.putExtra(AlarmClock.EXTRA_MESSAGE, "Test timer");
+                //alarmIntent.putExtra(AlarmClock.EXTRA_DAYS, Calendar.SATURDAY);
+
+                if (alarmIntent.resolveActivity(context.getPackageManager()) != null)
+                {
+                    context.startActivity(alarmIntent);
+                }
 
             }
         });
@@ -61,7 +152,41 @@ public class IntentGroup01 extends AppCompatActivity {
         btn05.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // set calendar event
+                // https://developer.android.com/reference/android/content/Intent#ACTION_INSERT
+                // https://stackoverflow.com/questions/14694931/insert-event-to-calendar-using-intent
+                // some smartphones require Intent.ACTION_EDIT instead of ACTION_INSERT
+                /*
+<intent-filter>
+        <action android:name="android.intent.action.INSERT" />
+        <data android:mimeType="vnd.android.cursor.dir/event" />
+        <category android:name="android.intent.category.DEFAULT" />
+    </intent-filter>
+                 */
+                Context context = v.getContext();
 
+                Calendar beginTime = Calendar.getInstance();
+                beginTime.set(2022, 2, 29, 7, 30);
+
+                Calendar endTime = Calendar.getInstance();
+                endTime.set(2022, 2, 31, 8, 30);
+
+                Intent intent = new Intent(Intent.ACTION_INSERT)
+                        .setData(CalendarContract.Events.CONTENT_URI)
+                        .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                                beginTime.getTimeInMillis())
+                        .putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                                endTime.getTimeInMillis())
+                        .putExtra(CalendarContract.Events.TITLE, "Yoga")
+                        .putExtra(CalendarContract.Events.DESCRIPTION, "Group class")
+                        .putExtra(CalendarContract.Events.EVENT_LOCATION, "The gym")
+                        .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY)
+                        .putExtra(Intent.EXTRA_EMAIL, "rowan@example.com,trevor@example.com");
+
+                if (intent.resolveActivity(context.getPackageManager()) != null)
+                {
+                    context.startActivity(intent);
+                }
             }
         });
 
